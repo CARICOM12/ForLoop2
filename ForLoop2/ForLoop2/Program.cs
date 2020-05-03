aafbdbf45ba508e6b6de64973c153a8e97ba1042
CARICOM12 <0190a@ukr.net>
2020-05-03 15:03:00 +0300

    Add files via upload

---
+++ ForLoop2/ForLoop2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ForLoop2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int number;
+            for (number = 0; number <= 100; number = number + 1)
+                Console.WriteLine("This is number: " + number);
+            Console.WriteLine("Bravo! Bravissimo!");
+        }
+    }
+}
